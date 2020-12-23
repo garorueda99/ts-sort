f771cdde20e856f202b413900ea8621bd53e8ae7
Andres Rueda <garo99@gmail.com>
2020-12-22 21:47:29 -0500

    sort v1

--- build/index.js
+++ build/index.js
@@ -1,3 +1,32 @@
 "use strict";
-var message = 'Hello World 3===>';
-console.log(message, 'hello');
+var array = [5, 0, 10, -1];
+console.log('this is the array to sort', array);
+// function sort(array: number[]): number[] {
+//   for (let entry of array) {
+//     console.log('this is entry:', entry);
+//   }
+//   return [1];
+// }
+// sort(array);
+var Sorter = /** @class */ (function () {
+    function Sorter(collection) {
+        this.collection = collection;
+    }
+    Sorter.prototype.sort = function () {
+        var length = this.collection.length;
+        for (var i = 0; i < length; i++) {
+            for (var j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    var leftHand = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = leftHand;
+                }
+            }
+        }
+    };
+    return Sorter;
+}());
+var sorter = new Sorter(array);
+console.log('This is the obj ', sorter.collection);
+sorter.sort();
+console.log('This is the obj sorted', sorter.collection);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,36 @@
-let message: string = 'Hello World 3===>';
+const array: number[] = [5, 0, 10, -1];
+console.log('this is the array to sort', array);
 
-console.log(message, 'hello');
+// function sort(array: number[]): number[] {
+//   for (let entry of array) {
+//     console.log('this is entry:', entry);
+//   }
+//   return [1];
+// }
+
+// sort(array);
+
+class Sorter {
+  collection: number[];
+
+  constructor(collection: number[]) {
+    this.collection = collection;
+  }
+  sort(): void {
+    const { length } = this.collection;
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const leftHand = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = leftHand;
+        }
+      }
+    }
+  }
+}
+
+const sorter = new Sorter(array);
+console.log('This is the obj ', sorter.collection);
+sorter.sort();
+console.log('This is the obj sorted', sorter.collection);
